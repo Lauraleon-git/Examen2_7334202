e51019fb69007ca3a44f5f4cf42ebd8d78101e68
Laura Leon <lauralglc93@gmail.com>
2024-04-06 09:35:21 -0400

    Examen

---
+++ Examen2/Examen.Api/Program.cs
@@ -0,0 +1,24 @@
+using Examen.Api;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+   // .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        var configuration = new ConfigurationBuilder()
+           .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+           .AddEnvironmentVariables()
+           .Build();
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddDbContext<Contexto>(options => options.UseSqlServer(
+                        configuration.GetConnectionString("cadenaConexion")));
+    })
+    .Build();
+
+host.Run();
